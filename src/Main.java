7e70c7c3f89c054a5b49fc2ebcf5d140b6b11cbc
NadezdaXa <nad3448@yandex.ru>
2022-08-22 23:24:30 +0400

    выполнила дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+
     }
-}
+
+    public static void task1() {
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+    }
+
+
+    public static void task2() {
+
+        var firstName = "Ivan";
+        var middleName = "Ivanovich";
+        var lastName = "Ivanov";
+
+        var fullName = String.format("%s %s %s", firstName, middleName, lastName);
+        System.out.printf("Данные ФИО сотрудника для заполнения отчета- %S %S %S", firstName, middleName, lastName);
+    }
+
+
+    public static void task3() {
+        System.out.println();
+
+        var fullName = "Иванов Семён Семёнович";
+        var result = fullName.replace("ё", "е");
+        System.out.printf("Данные ФИО сотрудника — %s",result);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
